705eae8d57afba6e4487141bafaef656f6f7d8b1
lucas kariuki <lucaskariuki@gmail.com>
2020-08-14 18:49:07 +0300

    added site functionality in add to cart, update cart and shipping address

--- store/models.py
+++ store/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 # Create your models here.
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=100, null=True)
     email = models.EmailField(blank=True, null=True)
 
@@ -36,6 +36,15 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems =self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
 
     @property
     def get_cart_total(self):
--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import (
 		store,
 		cart,
-		checkout
+		checkout,
+		updateItem,
+		processOrder
 	)
 
 app_name = "store"
@@ -10,5 +12,7 @@ app_name = "store"
 urlpatterns = [
 	path('', store, name = "store"),
 	path('cart/', cart, name = "cart"),
-	path('checkout/', checkout, name = "checkout")
+	path('checkout/', checkout, name = "checkout"),
+	path('update_item/', updateItem, name = "update_item"),
+	path('process_order/', processOrder, name = "process_order")
 ]
